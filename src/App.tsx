761e469899c8e01a0395431b900c2fe97b0ae55f
thiagoxporfirio <thiagovporfirio@gmail.com>
2025-03-19 09:48:47 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+      <iframe
+        src="/media_kit.pdf"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        title="PDF Viewer"
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
